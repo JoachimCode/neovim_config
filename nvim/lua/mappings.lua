393cfc46287651bac3c16b7091d5dba6c074433c
Joachim Duong <joachimduong@gmail.com>
2026-02-13 15:03:13 +0100

    updated

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,7 +5,9 @@ vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 
 
 local map = vim.keymap.set
-
+map("n", "<leader>h", function()
+  require("nvterm.terminal").toggle("float")
+end, { desc = "Toggle floating terminal" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
